e2bcde61fcbc680d2c09f8acb5a0a5cd3e2a71b4
Munirabonu <muniratillaxojayeva@gmail.com>
2023-01-12 14:53:37 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,153 @@
+// #html2
+// 1.Takrorlash, uyga vazifa
+// 2.Html attrebute => tegga qoshimcha vazifa yuklovchi qism 
+// vs content       => tegning ichida joylashgan narsalar
+//  vs element      => teg+content
+
+// 
+// inline => faqat oziga kk bolgan qism
+// block  => 
+// 3.Div vs Span 
+// div    => block .blocklarni orab turuvchi  
+// span   => inline. kichik bolimlar uchun 
+// 4.Link  a -> 
+// tel    => tel:234132 
+// email  => mailto
+// id     => bir page ichida harakatlanish
+// a href == home id  
+// *{ scroll-behavior: smooth;}
+
+// dowland => yuklash , value=>yuklash nomi
+// href   => manzil
+// target
+// 5.Img -> 
+// asosiy atribute arc alt
+// qoshimcha
+// width, height
+// 6.Video -> 
+// control =>  
+// loop => qayta qayta qoyish
+// autoplay => auto qoyish
+// muted  => ovozsiz
+// poster => oldindan chiquvchi rasm
+// width  => eni
+// height => boyi
+// 7.Audio -> control, loop, autoplay, muted.
+
+// 8.Iframe you tube => you tube da berilgan link orqali
+// 9.Iframe web site => src sayt manzili qoyiladi
+// target  => yangi bravzer sahifasida ochib beradi
+
+
+// shift+alt+f
+
+// inline & block
+
+// block
+// div h p 
+
+// inline
+// span i sub sup
+// a img video audio
+
+
+
+// lesson 3
+
+// 2.Table -> 
+// table => orab turuvchi
+// tr => table row table qatori
+// td => table katak ,table data'
+// th => table header bosh qism malumotlari 
+// td & th 
+// td => oddiy left
+// th => markaz bold
+// colspan => ustunlarni birlashtirish 
+// rowspan => qator birlashtirish
+
+//     Style: collapse => borderni bitta qilib beradi
+//  background => fon rangi 
+//  border => atribute katta qiymatlar qalin qilib beradi chegara chiziq
+//  color => text rangi
+//  text align => textni joylashuvi 
+//  cellpadding => border va content orasida masofa  
+// <!-- th => table header 
+// td => table data
+// row qator
+// column ustun
+// -->
+// thead tbody tfoot
+
+
+// 3.List -> order, unorder, description
+
+
+// Taglar: 
+// ol => tartiblan list
+// ul => unorder tartibsiz 
+// li => list item list bolimlari
+//  dl, dt, dd
+//  Attrebute; type => ol tegishli, list turi:raqam rim raqamlari harflar
+//  start =>nechinchi indexdan boshlanishi 
+
+// Style: list-style-type => css tomonidan list turini ozgartirish 
+//  circle, disc, none, latin, roman
+
+
+// <!--unorder list  ul 
+//   list item li
+//   order list ol
+// -->
+// <!-- 
+// inline 
+// 1.yonma yon
+// 2.width height qabul qilmaydi 
+// 3.faqat content joyini egallaydi
+
+// block 
+// 1.ketma ket
+// 2.widht height qabul qiladi 
+// 3. parentini 100% egallaydi 
+
+// inline-block 
+// 1.yonma yon
+// 2.widht height qabul qiladi 
+// 3.faqat content joyini egallaydi agar width berilsa width ozgaradi 
+//  -->
+
+
+// 2.Input: 
+// text => matn
+// password => arol, dumaloq holatda bolib qoladi
+// email  => email @gmail.com   
+// number => son, . , e   
+// checkbox => kop malumotlar ber nechta
+// radio  =>  2tadan bitta ortga qaytish yoq
+//  bir nechta radiodan faqat bittasini olish  
+// name bir xil 
+// color,range, date, time, file, 
+// reset => barchasi ochiradi
+// submit => jonatish 
+// Attrebute: type =>input turi
+// placeholder => input haqida malumot ,avtomatik ochadi 
+// value => qiymat 
+// minlength => min uzunlik 
+// maxlength => max uzunlik 
+// required  => ogohlantirish 
+// readonly  => faqat oqish uchun, focus qilsa boladi,bazaga value ketadi 
+// disabled  => value ketmaydi, bosh boradi
+// id   => input kaliti 
+// name => input nomi
+// bravzer name
+// pattern => qiymatlar fixed
+// step    => osib borish darajasi
+// 3.Label => tashqaridan input haqida malumot
+// label+input
+// input id == label for 
+// 4.Form -> action.
+// 5.textarea -> rows, cols
+// 6.Select -> option, value.
+// tanlov imkoniyati , asosan til tanlash 
+// 7.Data list +input => 
+// datalist id == input list 
+
